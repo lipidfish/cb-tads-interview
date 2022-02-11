58df8197127e36591e60c83c378b9485aa358006
tpeterson <tpeterson@LAPTOP26>
2022-02-10 18:34:37 -0600

    cleanup

--- UnitTests/HtmlWriterTests.cs
+++ UnitTests/HtmlWriterTests.cs
@@ -19,6 +19,19 @@ namespace UnitTests
         }
 
         [TestMethod]
+        public void Button_OnRender_Works_2()
+        {
+            // TODO: make changes to the Button class that make the following test pass
+            var button = new Button()
+            {
+                Text = "Something Else"
+            };
+            const string expected = "<a class=\"btn btn-primary\">Something Else</a>";
+            Assert.AreEqual(expected, button.Render());
+        }
+
+
+        [TestMethod]
         public void Button_OnRender_Id_Class_Works()
         {
             // TODO: make changes to the Button class that make the following test (and all previous tests) pass
@@ -33,6 +46,35 @@ namespace UnitTests
         }
 
         [TestMethod]
+        public void Button_OnRender_Id_Class_Works_2()
+        {
+            // TODO: make changes to the Button class that make the following test (and all previous tests) pass
+            var button = new Button()
+            {
+                Id = "thisButton2",
+                Style = "display: none;",
+                Text = "Something"
+            };
+            const string expected = "<a class=\"btn btn-primary\" id=\"thisButton2\" style=\"display: none;\">Something</a>";
+            Assert.AreEqual(expected, button.Render());
+        }
+
+        [TestMethod]
+        public void Button_OnRender_Id_Class_Works_3()
+        {
+            // TODO: make changes to the Button class that make the following test (and all previous tests) pass
+            var button = new Button()
+            {
+                Id = "thisButton",
+                Style = "display: none;",
+                Text = "Something Else"
+            };
+            const string expected = "<a class=\"btn btn-primary\" id=\"thisButton2\" style=\"display: none;\">Something Else</a>";
+            Assert.AreEqual(expected, button.Render());
+        }
+
+
+        [TestMethod]
         public void Button2_OnRender_Works()
         {
             // TODO: make changes to the Button2 class that make the following test (and all previous tests) pass
@@ -43,14 +85,38 @@ namespace UnitTests
         }
 
         [TestMethod]
+        public void Button2_OnRender_Works_2()
+        {
+            // TODO: make changes to the Button2 class that make the following test (and all previous tests) pass
+            var button = new Button2();
+            // should be able to add the "Something Else" string through a property
+            const string expected = "<button class=\"btn btn-primary\">Something Else</button>";
+            Assert.AreEqual(expected, button.Render());
+        }
+
+
+        [TestMethod]
         public void Button2_OnRender_Id_Class_Works()
         {
             // TODO: make changes to the Button2 class that make the following test (and all previous tests) pass
             var button = new Button2();
-            // should be able to add the "something" text, id attribute and style attribute through properties
+            // should be able to add the "Something" text, id attribute and style attribute through properties
             const string expected = "<button class=\"btn btn-primary\" id=\"thisButton\" style=\"display: none;\">Something</button>";
             Assert.AreEqual(expected, button.Render());
         }
+
+
+        [TestMethod]
+        public void Button2_OnRender_Id_Class_Works_2()
+        {
+            // TODO: make changes to the Button2 class that make the following test (and all previous tests) pass
+            var button = new Button2();
+            // should be able to add the "Something Else" text, id attribute and style attribute through properties
+            const string expected = "<button class=\"btn btn-primary\" id=\"thisButton2\" style=\"display: none;\">Something Else</button>";
+            Assert.AreEqual(expected, button.Render());
+        }
+
+
 
         [TestMethod]
         public void Input_OnRender_Id_Class_Works()
@@ -63,6 +129,20 @@ namespace UnitTests
                 Placeholder = "Default Text"
             };
             const string expected = "<input class=\"btn btn-primary\" id=\"thisInput\" style=\"display: none;\" placeholder=\"Default Text\"></input>";
+            Assert.AreEqual(expected, input.Render());
+        }
+
+        [TestMethod]
+        public void Input_OnRender_Id_Class_Works_2()
+        {
+            // TODO: make changes to the Input class that make the following test (and all previous tests) pass
+            var input = new Input()
+            {
+                Id = "thisInput",
+                Style = "display: none;",
+                Placeholder = "Default Text 2"
+            };
+            const string expected = "<input class=\"btn btn-primary\" id=\"thisInput\" style=\"display: none;\" placeholder=\"Default Text 2\"></input>";
             Assert.AreEqual(expected, input.Render());
         }
 
